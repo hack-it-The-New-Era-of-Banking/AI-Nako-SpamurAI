81cf3dbcc7e4b05d5afda494674085d3a76bcc9e
uniqyldev <brencastillo@proton.me>
2025-04-04 22:15:06 +0800

    nugagawen pag nag commit sa main?

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,20 @@
-// index.js
 const express = require("express");
+const cors = require("cors");
 const { exec } = require("child_process");
 const app = express();
 const port = 3000;
+const supabaseClient = require("../database/supabaseClient");
+
+// Initialize Supabase Client
+
+// Configure CORS middleware to allow all origins (customize as needed)
+app.use(
+  cors({
+    origin: "*", // Allow all origins. You can restrict to specific domains if needed.
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.get("/run-script", (req, res) => {
   console.log("DEBUG: Received request to run script.");
@@ -18,6 +30,13 @@ app.get("/run-script", (req, res) => {
     console.log("DEBUG: Script stdout output:", stdout);
     res.send(`Script output: ${stdout}`);
   });
+});
+
+// Request to fetch forensic PDFs
+app.get("/fetch-forensic-pdfs", (req, res) => {
+  console.log("DEBUG: Received request to fetch forensic PDFs.");
+  // Add your logic to fetch forensic PDFs here
+  res.send("Fetching forensic PDFs...");
 });
 
 app.listen(port, () => {
